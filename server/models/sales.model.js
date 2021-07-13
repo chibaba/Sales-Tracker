9811c9de9b7346d369306b8f2416d76232ed70f5
chi <chinedu.onyeoka@gmail.com>
2021-07-13 21:11:18 +0100

    created model for sales

---
+++ server/models/sales.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+const SalesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: 'Title is required'
+  },
+  category: {
+    type: String,
+    trim: true,
+    required: 'Category is required'
+  },
+  amount: {
+    type: Number,
+    min: 0,
+    required: 'Amount is required'
+  },
+  sold_on: {
+    type: Date,
+    default: Date.now()
+  },
+  notes: {
+    type: String,
+    trim: true
+  },
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now()
+  },
+  recorded_by: {type: mongoose.Schema.ObjectId, ref: 'User'}
+})
+export default mongoose.model('Sales', SalesSchema)
