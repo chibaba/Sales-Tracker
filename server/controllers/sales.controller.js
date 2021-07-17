11fd50a5513cb9aead26b4e025d378c9e325a962
chi <chinedu.onyeoka@gmail.com>
2021-07-17 21:06:48 +0100

    wip

--- server/controllers/sales.controller.js
+++ server/controllers/sales.controller.js
@@ -99,3 +99,53 @@ today.setUTCHours(0,0,0,0)
     return res.status(400).json({error: errorHandler.getErrorMessage(err)})
   }
 }
+
+const salesByCategory = async(req, res) => {
+  const date = new Date(), y = date.getFullYear(), m = date.getMonth()
+  const firstDay = new Date(y, m, 1)
+  const lastDay = new Date(y, m + 1, 0)
+
+  try {
+    let categoryMonthlyAvg = await Sales.aggregate([
+      {
+        $facet: {
+          average: [
+            {$match:  {recorded_by: mongoose.Types.ObjectId(req.auth._id)}},
+            {$group: { _id: {category: "$category", month: {$month: "$incured_on"}}, totalBought: {$sum: "$amount"}}},
+            { $group: { _id: "$_id.category", avgBought: {$avg: "$totalBought"}}},
+            {
+              $project: { 
+                _id: "$_id", value: {average: "$avgBought"}
+              }
+            }
+          ],
+          total: [
+            { $match : { sold_on :{ $gte : firstDay, $lte : lastDay }, recorded_by: mongoose.Types.
+          ObjectId(req.auth._id) }},
+          { $group : { _id: "$category", totalBought: {$sum: "$amount"}}},
+          {
+            $project: {
+              _id: "$_id", value: {total: "totalSpent"},
+            }
+          }
+        ]
+      }
+    },
+          {
+            $project: { overview:  { $setUnion: ['$average', '$total']},
+          }
+          },
+          {$unwind: '$overview'},
+          {$replaceRoot: { newRoot: "$overview"}},
+          { $group: { _id: "$_id", mergedValues: {$mergeObjects: "$value"}}}
+          ]).exec()
+          res.json(categoryMonthlyAvg)
+        }catch (err) {
+          console.log(err)
+          return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+          })
+        }
+      }
+
+      
