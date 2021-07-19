6a7975440492fde7c807170c0b6a42f52a376c62
chi <chinedu.onyeoka@gmail.com>
2021-07-19 14:36:19 +0100

    router

--- server/routes/sales.routes.js
+++ server/routes/sales.routes.js
@@ -9,3 +9,24 @@ router.route('/api/sales/current/preview')
 
 router.route('/api/sales/by/category')
 .get(authCtrl.requireSignin, salesCtrl.salesByCategory)
+
+router.route('/api/sales/plot')
+.get(authCtrl.requireSignin, salesCtrl.plotSales)
+
+router.route('/api/sales/averages')
+.get(authCtrl.requireSignin, salesCtrl.averageCategories)
+
+router.route('/api/sales/yearly')
+.get(authCtrl.requireSignin, salesCtrl.yearlySales)
+
+router.route('/api/sales')
+.post(authCtrl.requireSignin, salesCtrl.create)
+.get(authCtrl.requireSignin, salesCtrl.listByUser)
+
+router.route('/api/sales/:salesId')
+.put(authCtrl.requireSignin, salesCtrl.hasAuthorization, salesCtrl.update)
+.delete(authCtrl.requireSignin, salesCtrl.hasAuthorization, salesCtrl.remove)
+
+router.param('salesId', salesCtrl.salesByID)
+
+export default router
