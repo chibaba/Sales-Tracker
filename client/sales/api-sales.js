bb49a82217dd42e39c5eb0ebad2f03c77b7b622b
chi <chinedu.onyeoka@gmail.com>
2021-07-23 21:50:15 +0100

    api integration

--- client/sales/api-sales.js
+++ client/sales/api-sales.js
@@ -34,3 +34,52 @@ const listByUser = async(params, credentials, signal) => {
     console.log(err)
   }
 }
+
+const currentMonthPreview = async (credentials, signal) => {
+  try {
+    let response = await fetch('/api/sales/current/preview', {
+      method: 'GET',
+      signal: signal,
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    })
+    return await response.json()
+  }catch(err){
+    console.log(err)
+  }
+}
+
+const salesByCategory = async (credentials, signal) => {
+  try {
+    let response = await fetch('/api/sales/by/category', {
+      method: 'GET',
+      signal: signal,
+      headers:{
+        'Accept':'application/json',
+        'Authorization':'Bearer ' + credentials.t
+      }
+    })
+    return await response.json()
+  } catch(err){
+    console.log(err)
+  }
+}
+
+const yearlySales = async (params, credentials, signal) => {
+  const query = queryString.stringify(params)
+  try {
+    let response = await fetch('/api/sales/yearly?'+query, {
+      method: 'GET',
+      signal: signal,
+      headers: { 
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    })
+    return await response.json()
+  }catch(err) {
+    console.log(err)
+  }
+}
