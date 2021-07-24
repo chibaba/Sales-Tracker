6ec656aac53f129e086c72289e541dbbfd9bc71b
chi <chinedu.onyeoka@gmail.com>
2021-07-24 20:29:09 +0100

    mt ui

--- client/sales/Sales.js
+++ client/sales/Sales.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { makeStyle } from  '@material-ui/core/styles';
+import { makeStyles } from  '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import  ExpansionPanelSummary  from '@material-ui/core/ExpansionPanelSummary';
 import  ExpansionPanelDetails  from '@material-ui/core/ExpansionPanelDetails';
@@ -16,3 +16,30 @@ import Icon  from '@material-ui/core/Icon'
 import {Redirect} from 'react-router-dom'
 import DateFnsUtils from '@date-io/date-fns/build/date-fns-utils';
 import { DateTimePicker, DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers"
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '90%',
+    maxWidth: '800px',
+    margin: 'auto',
+    marginTop: '40',
+    marginBottom: '40'
+  },
+  heading: {
+    fontSize: '1.5em',
+    fontWeight: theme.typography.fontWeightRegular,
+    marginTop: 12,
+    marginBottom: 4
+  },
+  error: {
+    verticalAlign: 'middle',
+  },
+  notes: {
+      color: 'grey'
+  },
+  panel: {
+    border: '1px solid #58cd3d',
+    margin: 6
+  }
+}))
