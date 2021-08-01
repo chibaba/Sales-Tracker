cf384ffddd68a80ca0f48b2d71815310d0e71755
chi <chinedu.onyeoka@gmail.com>
2021-08-01 20:08:59 +0100

    wip

--- template.js
+++ template.js
@@ -7,7 +7,8 @@ export default ({markup, css}) => {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
         >
-        <title>MERN Skeleton</title>
+        <title>Sales Tracker
+        </title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <style>
